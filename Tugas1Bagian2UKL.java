c24f68f64fa485b424b6db4402f3d056c1b93c65
barrafdq <barra_dhiayaulhaq_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 12:44:49 +0700

    Add files via upload

---
+++ Tugas1Bagian2UKL.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Tugas1Bagian2UKL {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah bilangan: ");
+        int bilangan = input.nextInt();
+
+        if (bilangan % 2 == 0) {
+            System.out.println("Bilangan " + bilangan + " adalah GENAP.");
+        } else {
+            System.out.println("Bilangan " + bilangan + " adalah GANJIL.");
+        }
+    }
+}
+
+
